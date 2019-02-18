44daf8a46ca4c54670c9528cbbb11ba2f2f91a4f
SabrinaGear <sabrinagear.ft@gmail.com>
2019-02-18 03:37:14 -0600

    changes to dev dependencies

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,4 @@
-const dbEnvironment = process.env.DB_ENVIRONMENT || "development";
+const dbEnvironment = process.env.DB || "development";
 const knex = require("knex");
 const knexConfig = require("../knexfile");
 module.exports = knex(knexConfig[dbEnvironment]);
