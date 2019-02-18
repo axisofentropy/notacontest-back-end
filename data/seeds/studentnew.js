69eb31668a4c0940e95015807d484c5516ec0322
SabrinaGear <sabrinagear.ft@gmail.com>
2019-02-18 04:42:49 -0600

    debugging

--- data/seeds/studentnew.js
+++ data/seeds/studentnew.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("students")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("students").insert([
